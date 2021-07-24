fd6a8d5d4d8bfe9b78552de5199bde50f76476f5
asn-athira <athira@rightsolution.ae>
2021-07-24 16:55:26 +0530

    modified

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -13,12 +13,12 @@ development: {
   }
 },
 production: {
- DB_URL=ec2-34-233-114-40.compute-1.amazonaws.com
- DB_NAME=d56ah1es5shct9
- DB_USERNAME=wlxblxmtviynbz
- DB_PASSWORD=b0643dc4a292fd9acd24862aee5187a4fbdfb94085724a134dccd23600e4c30f
- DB_PORT=5432
- DATABASE_URL=postgres://wlxblxmtviynbz:b0643dc4a292fd9acd24862aee5187a4fbdfb94085724a134dccd23600e4c30f@ec2-34-233-114-40.compute-1.amazonaws.com:5432/d56ah1es5shct9
+ HOST: "ec2-34-233-114-40.compute-1.amazonaws.com",
+ DB: "d56ah1es5shct9",
+ USER: "wlxblxmtviynbz",
+ PASSWORD: "b0643dc4a292fd9acd24862aee5187a4fbdfb94085724a134dccd23600e4c30f",
+ DB_PORT: 5432,
+ URI: "postgres://wlxblxmtviynbz:b0643dc4a292fd9acd24862aee5187a4fbdfb94085724a134dccd23600e4c30f@ec2-34-233-114-40.compute-1.amazonaws.com:5432/d56ah1es5shct9"
 }
 };
 module.exports = config;
