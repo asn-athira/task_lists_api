5eab0e2f8e1a9af06b3bb28f09503418bb728e80
asn-athira <athira@rightsolution.ae>
2021-07-26 17:02:54 +0530

    fixed

--- server.js
+++ server.js
@@ -20,13 +20,17 @@ app.use(express.json());  /* bodyParser.json() is deprecated */
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
 
+app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      next();
+    });
+
 const db = require("./app/models");
 db.sequelize.sync();
 // // drop the table if it already exists
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db.");
 // });
-
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Task list application." });
